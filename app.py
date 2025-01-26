6729d56be06ac739e9c05677040cdb4c2b0a7f26
Atul Kumar <tasneatul@gmail.com>
2025-01-26 17:16:31 +0530

    new chmages for educatuin and profiles

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html'),200
 
 @app.route('/portfolio')
 def portfolio():
-    return render_template('portfolio.html')
+    return render_template('portfolio.html'),200
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port,debug=True)
