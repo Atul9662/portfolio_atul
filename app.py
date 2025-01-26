0ddf7e40f1174f03a3a909688d6502589124f012
Atul Kumar <tasneatul@gmail.com>
2025-01-26 13:12:08 +0530

    port dynamic

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 print("this is main app")
 from flask import Flask, render_template
+import os
+
 
 app = Flask(__name__)
 
@@ -12,4 +14,5 @@ def portfolio():
     return render_template('portfolio.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
